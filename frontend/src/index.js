51c0c97c89a209fc0178e004a0756d8298965068
yamuna <yamuna.pdy@gmail.com>
2025-05-30 22:43:24 +0300

    updated users

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,8 @@ import createStore from './reducks/store/store';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
+// import { ConnectedRouter } from 'connected-react-router';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import * as History from 'history';
 
@@ -14,9 +15,15 @@ export const store = createStore(history);
 ReactDOM.render(
 
    <Provider store={store}>
-    <ConnectedRouter history={history}>
+    {
+      <BrowserRouter>
       <App />
-    </ConnectedRouter>
+    </BrowserRouter>
+    
+    /* <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter> */
+    }
   </Provider>,
   document.getElementById('root')
 );
